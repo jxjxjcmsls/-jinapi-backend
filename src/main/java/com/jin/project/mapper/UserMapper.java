76f37d04f23df3251d0b19284c3b8cb1a111ee42
“programmer-xiaojin” <jxw13633973802@163.com>
2024-06-16 15:17:41 +0800

    使用后端项目模板

---
+++ src/main/java/com/jin/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.jin.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jin.project.model.entity.User;
+
+/**
+ * @Entity com.jin.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
